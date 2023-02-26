bd35163a1ec93929752a96db871f269295c79ea2
Ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2023-02-26 11:07:26 +0500

    lazy component has removed from app.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import React, { Suspense, lazy } from 'react';
-
-const LazyComp = lazy(() => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(import('./components/lazy/Lazy'));
-    }, 5000);
-  });
-});
-
 function App() {
   return (
     <div className='App'>
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <LazyComp />
-      </Suspense>
+      <h1>wassup man</h1>
     </div>
   );
 }
--- src/components/lazy/Lazy.jsx
+++ src/components/lazy/Lazy.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+// import React from 'react';
 
 const LazyComp = () => {
   return <div>LazyComp</div>;
 };
+
+// // app.js's code for lazy loading
+// const LazyComp = lazy(() => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(import('./components/lazy/Lazy'));
+//     }, 5000);
+//   });
+// });
+// // app.js's code for lazy loading
 
 export default LazyComp;
